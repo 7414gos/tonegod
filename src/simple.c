f056352dc55e20b416e7be7c2bd8171fc06ee00b
7414gos <109703068@nccu.edu.tw>
2022-11-01 12:28:04 +0000

    os

--- src/simple.c
+++ src/simple.c
@@ -7,13 +7,14 @@
 // 當模組被載入會觸發這個函式 返回0代表成功 其他值代表失敗
 static int simple_init(void)
 {
+    unsigned long int jif = jiffies;
+    int hztime = HZ;
     printk(KERN_INFO "Loading Module\n");
-    
     //////////////////////
     ////<在此加入程式碼>////
     /////////////////////
-    // printk(KERN_INFO "");  印出載入時的jiffies，使用％lu來表示，格式為"init_jiffies: XXXXXXXXXXX"，記得換行
-    // printk(KERN_INFO "");  印出系統定義的的HZ，使用％d來表示，格式為"HZ: XXX"，記得換行
+       printk(KERN_INFO "init_jiffies: %lu\n",jif); // 印出載入時的jiffies，使用％lu來表示，格式為"init_jiffies: XXXXXXXXXXX"，記得換行
+       printk(KERN_INFO "HZ: %d\n",hztime); // 印出系統定義的的HZ，使用％d來表示，格式為"HZ: XXX"，記得換行
        return 0;
 }
 
@@ -23,7 +24,9 @@ static void simple_exit(void) {
     //////////////////////
     ////<在此加入程式碼>////
     /////////////////////>
-    //printk(KERN_INFO "");  印出移除時的jiffies，使用％lu來表示，格式為"exit_jiffies: XXXXXXXXXXX"，記得換行
+        long time;
+	time  = jiffies;
+        printk(KERN_INFO "exit_jiffies: %lu\n",time); // 印出移除時的jiffies，使用％lu來表示，格式為"exit_jiffies: XXXXXXXXXXX"，記得換行
 	printk(KERN_INFO "Removing Module\n");
 }
 
